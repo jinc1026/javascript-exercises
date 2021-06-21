cf59a63f8cfec2a93046ca1db5faa54dad5650ac
Jin Choe <jinc1026@hotmail.com>
2021-06-21 07:23:04 +0000

    Modified js files to complete the exercises

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(inputString, iteration) {
 
+	let completedString="";
+	
+	if (iteration < 0){
+		return 'ERROR'
+	} else {
+		for (let i=0; i<iteration; i++){
+			completedString = completedString.concat(inputString);
+		}
+	}
+	
+	return completedString;
 };
 
 module.exports = repeatString;
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+	var completedString="";
+	
+	for (var i=inputString.length; i>=0; i--){
+		completedString += inputString.charAt(i);
+	}
+	
+	return completedString;
 };
 
 module.exports = reverseString;
